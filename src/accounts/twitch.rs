a0a1860b12a73b4bfcbc06e08e03276491b53c7f
PeakKS <bbatson101@gmail.com>
2025-02-20 21:06:49 -0500

    Twitch: Implement OAuth2 auto-refresh with token saved in keyring

--- src/accounts/twitch.rs
+++ src/accounts/twitch.rs
@@ -1,6 +1,14 @@
-use std::str::FromStr;
+use std::{
+    str::{FromStr, Utf8Error},
+    string::FromUtf8Error,
+};
 
-use twitch_api::twitch_oauth2::{self, url, ClientId, DeviceUserTokenBuilder, Scope};
+use keyring::Entry;
+use twitch_api::twitch_oauth2::{
+    self,
+    tokens::errors::{DeviceUserTokenExchangeError, RefreshTokenError},
+    url, AccessToken, ClientId, DeviceUserTokenBuilder, RefreshToken, Scope, UserToken,
+};
 
 use gtk::{
     gio::{ffi::GAsyncReadyCallback, Cancellable},
@@ -11,43 +19,119 @@ use reqwest;
 
 use crate::{config, runtime, window::StreamsWindow};
 
+const KEYRING_SERVICE: &str = "streams";
+const KEYRING_USER: &str = "twitch-refresh";
+
+const TWITCH_SCOPES: &[Scope] = &[Scope::UserReadFollows];
+
+#[derive(Debug)]
+enum TokenError {
+    OAuthError(DeviceUserTokenExchangeError<reqwest::Error>),
+    TokenParseError(FromUtf8Error),
+    RefreshError(RefreshTokenError<reqwest::Error>),
+    KeyringError(keyring::Error),
+}
+
+impl From<DeviceUserTokenExchangeError<reqwest::Error>> for TokenError {
+    fn from(value: DeviceUserTokenExchangeError<reqwest::Error>) -> Self {
+        Self::OAuthError(value)
+    }
+}
+
+impl From<FromUtf8Error> for TokenError {
+    fn from(value: FromUtf8Error) -> Self {
+        Self::TokenParseError(value)
+    }
+}
+
+impl From<RefreshTokenError<reqwest::Error>> for TokenError {
+    fn from(value: RefreshTokenError<reqwest::Error>) -> Self {
+        Self::RefreshError(value)
+    }
+}
+
+impl From<keyring::Error> for TokenError {
+    fn from(value: keyring::Error) -> Self {
+        Self::KeyringError(value)
+    }
+}
+
 pub fn sign_in() {
+    runtime().spawn(async move {
+        println!("Refreshing Twitch access token...");
+        let access_token = match match refresh_token().await {
+            Ok(access_token) => Ok(access_token),
+            Err(error) => {
+                eprintln!("Failed to refresh token: {error:?}");
+                println!("Getting new access token...");
+                get_new_token().await
+            }
+        } {
+            Ok(access_token) => access_token,
+            Err(error) => {
+                eprintln!("Failed to get new token: {error:?}");
+                return;
+            }
+        };
+
+        println!("Got access token successfully");
+    });
+}
+
+async fn refresh_token() -> Result<AccessToken, TokenError> {
     let client_id = ClientId::new(String::from(config::TWITCH_CLIENT_ID));
-    let mut builder = DeviceUserTokenBuilder::new(client_id, vec![Scope::UserReadFollows]);
+    let entry = Entry::new(KEYRING_SERVICE, KEYRING_USER).unwrap();
+    let saved_refresh_token = entry.get_secret()?;
+    let saved_refresh_token = String::from_utf8(saved_refresh_token)?;
+    let refresh_token = RefreshToken::from(saved_refresh_token);
 
     let reqwest = reqwest::Client::builder()
         .redirect(reqwest::redirect::Policy::none())
         .build()
         .unwrap();
 
-    runtime().spawn(async move {
-        let code_response = match builder.start(&reqwest).await {
-            Ok(code_response) => code_response,
-            Err(error) => {
-                eprintln!("{error}");
-                return;
-            }
-        };
+    // TODO: Schedule a task that keeps access_token refreshed before duration expires
+    let (access_token, _duration, new_refresh_token) = refresh_token
+        .refresh_token(&reqwest, &client_id, None)
+        .await?;
 
-        let verification_uri = code_response.verification_uri.clone();
-        glib::spawn_future(async move {
-            gtk::UriLauncher::new(&verification_uri).launch(
-                None::<&StreamsWindow>,
-                Cancellable::NONE,
-                |_| {
-                    println!("Opened link");
-                },
-            );
-        });
+    if let Some(new_refresh_token) = new_refresh_token {
+        entry.set_secret(new_refresh_token.secret().as_bytes())?;
+    } else {
+        eprintln!("Did not receive a new refresh token after refreshing");
+    }
 
-        let mut token = match builder.wait_for_code(&reqwest, tokio::time::sleep).await {
-            Ok(token) => token,
-            Err(error) => {
-                eprintln!("{error}");
-                return;
-            }
-        };
+    Ok(access_token)
+}
 
-        println!("Got token: {token:?}");
+async fn get_new_token() -> Result<AccessToken, TokenError> {
+    let client_id = ClientId::new(String::from(config::TWITCH_CLIENT_ID));
+    let mut builder = DeviceUserTokenBuilder::new(client_id, TWITCH_SCOPES.to_vec());
+
+    let reqwest = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .unwrap();
+
+    let code_response = builder.start(&reqwest).await?;
+
+    let verification_uri = code_response.verification_uri.clone();
+    glib::spawn_future(async move {
+        gtk::UriLauncher::new(&verification_uri).launch(
+            None::<&StreamsWindow>,
+            Cancellable::NONE,
+            |_| {
+                println!("Opened link");
+            },
+        );
     });
+
+    let token = builder.wait_for_code(&reqwest, tokio::time::sleep).await?;
+
+    if let Some(refresh_token) = token.refresh_token {
+        let entry = Entry::new(KEYRING_SERVICE, KEYRING_USER).unwrap();
+        entry.set_secret(refresh_token.secret().as_bytes())?;
+    }
+
+    Ok(token.access_token)
 }
